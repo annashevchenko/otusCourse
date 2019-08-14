f4387d389e9257a36396e9e7e525eac174e23d5e
annashevchenko <shevchenko.anna.s@gmail.com>
2019-08-14 19:56:59 +0300

    Домашнее задание №4 поправила тест test_api_response_OK

--- 4_home_work/test_by_homeWork4_API_response_OK.py
+++ 4_home_work/test_by_homeWork4_API_response_OK.py
@@ -4,10 +4,5 @@ import pytest
 def test_api_response_ok(api_client):
     """Description: тест по домашнему заданию № 4   проверяем, что по url возвращается код 200"""
     res = api_client.get()
-    if res.status_code == 200:
-        result = True
-    else:
-        result = False
-    return result
     # Проверяем что возвращаемые параметры верны
-    assert result is True
+    assert res.status_code == 200
