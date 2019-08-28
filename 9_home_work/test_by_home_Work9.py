13c554f8ed71f5c591ee352a282ca9570b9e7426
annashevchenko <shevchenko.anna.s@gmail.com>
2019-08-28 21:03:30 +0300

    Реализовать скрипт работы с drag n drop Цель: Научиться работать с ActionChains 1. Зайти на сайт https://code.makery.ch/library/dart-drag-and-drop/ 2. В первом примере сайта (Examples - Basic) сложить все иконки документов в корзинку

---
+++ 9_home_work/test_by_home_Work9.py
@@ -0,0 +1,13 @@
+import pytest
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+def test_use_drag_and_drog(driver):
+    """Тест использует инструмент drag_and_drop для переноса в корзину элементов"""
+    # находим корзину, в которую будут перенесены элементы
+    element_trash = driver.find_element_by_css_selector("div[class='trash']")
+    # выполняем цикл для каждого найденного элемента выполняем перенос в корзину через drag_and_drop
+    # и проверяем, что корзина полная после каждого переноса
+    for element in driver.find_elements_by_css_selector("div[class='container'] img"):
+        ActionChains(driver).drag_and_drop(element, element_trash).perform()
+        driver.find_element_by_css_selector("div[class='trash full']")
